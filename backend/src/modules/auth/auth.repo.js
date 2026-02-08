54e1273a6d7d3c04e14159427682bbcb09092812
Hardik Agarwal <hardikagarwal9308@gmail.com>
2026-02-08 19:08:32 +0530

    refactor: remove unnecessary PrismaClient instantiation in auth repository

--- backend/src/modules/auth/auth.repo.js
+++ backend/src/modules/auth/auth.repo.js
@@ -1,6 +1,4 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import prisma from "../../config/database.js";
 
 export const findUserByEmail = (email) => {
   return prisma.user.findUnique({
