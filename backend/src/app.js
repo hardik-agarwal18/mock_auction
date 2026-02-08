f3ea316836b3a8c239da490bf1b3f7746017c6a5
Hardik Agarwal <hardikagarwal9308@gmail.com>
2026-02-08 19:57:24 +0530

    feat: add morgan for logging HTTP requests

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
+import morgan from "morgan";
 import authRoutes from "./modules/auth/auth.routes.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.use("/api/auth", authRoutes);
 
