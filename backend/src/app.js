30828fbb3fb83269fd15b09b1f8482aad3271e43
Hardik Agarwal <hardikagarwal9308@gmail.com>
2026-02-09 16:32:33 +0530

    feat: add global error handling middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import authRoutes from "./modules/auth/auth.routes.js";
+import errorMiddleware from "./shared/middleware/error.middleware.js";
 
 const app = express();
 
@@ -12,5 +13,8 @@ app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+// Global error handler (LAST)
+app.use(errorMiddleware);
 
 export default app;
