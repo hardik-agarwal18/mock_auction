7996f48be632d1e31b95778f64521465bd820fa4
Hardik Agarwal <hardikagarwal9308@gmail.com>
2026-02-11 00:00:48 +0530

    feat: implement player service with addPlayerService and getPlayersService functions

---
+++ backend/src/modules/player/player.service.js
@@ -0,0 +1,35 @@
+import { findPlayersByRoom } from "./player.repo";
+import { findRoomById } from "../room/room.repo.js";
+import { createPlayer } from "./player.repo.js";
+import AppError from "../../shared/errors/appError.js";
+
+export const addPlayerService = async (userId, roomId, data) => {
+  const room = await findRoomById(roomId);
+
+  if (!room) {
+    throw new AppError("Room not found", 404);
+  }
+
+  if (room.hostId !== userId) {
+    throw new AppError("Only host can add players", 403);
+  }
+
+  if (room.status !== "WAITING") {
+    throw new AppError("Cannot add players after auction starts", 400);
+  }
+
+  return createPlayer({
+    ...data,
+    roomId,
+  });
+};
+
+export const getPlayersService = async (roomId) => {
+  const room = await findRoomById(roomId);
+
+  if (!room) {
+    throw new AppError("Room not found", 404);
+  }
+
+  return findPlayersByRoom(roomId);
+};
